c2d9a2bd458a395f67bc40bb3322d9cdfea30746
Yosary <yl_santosp@unicah.edu>
2021-06-27 19:26:27 -0600

    Creacion de Screens

---
+++ src/components/screens/Categories.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+export const CategoriaScreen=()=>{
+  return (
+    <View>
+      <Text>Categori!</Text>
+    </View>
+  );
+}
---
+++ src/components/screens/Region.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import {Text,View} from "react-native";
+
+export const RegionScreen =()=>{
+    return (
+      <View>
+        <Text>Region!</Text>
+      </View>
+    );
+  }
