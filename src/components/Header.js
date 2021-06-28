477f54fb6fd6d7f9fb63dce2b51b696b40fb3f06
nicollh9 <nicollarita06@gmail.com>
2021-06-27 18:26:32 -0600

    Actualizacion de AppBar.Header con logotipo

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { Appbar } from 'react-native-paper';
+import { Image,StyleSheet,StatusBar} from 'react-native';
+
+const Header = () => {
+
+  return (
+    <Appbar.Header style={styles.header}> 
+      <StatusBar></StatusBar>
+       <Image
+        style={{ width: 115, height: 115, marginLeft:115}}
+        source={require('../../assets/AppEatWell.png')} />
+    </Appbar.Header>
+  );
+};
+
+const styles = StyleSheet.create({
+  header:{
+    backgroundColor: '#ff5722',
+  },
+})
+
+export default Header;
+
+
+  
