a6a80b57866e132e07011e468d29b45a5087d8fa
nicollh9 <nicollarita06@gmail.com>
2021-06-27 18:27:11 -0600

    Importación de Header para pantalla principal

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { ScrollView, StyleSheet, View} from "react-native";
 import CardList from "../RandomList";
 import { fetcheatWell } from "../../api";
+import Header from "../Header";
 
 const Home = () => {
 const [recetas, setrecetas] = useState({});
@@ -18,6 +19,7 @@ const [recetas, setrecetas] = useState({});
 
   return (
     <ScrollView>
+      <Header/>
       <View>
           <>{recetas.meals ? <CardList randomR={recetas}/> : null}</>
       </View>
