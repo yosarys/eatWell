e77f715675f09953fb5a1e8ef3087088aeee1498
nicollh9 <nicollarita06@gmail.com>
2021-06-30 00:05:22 -0600

    Filtrado para recetas de regiones

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import getEnvVars from "../../environment";
 
 // Rutas de la API
-const {RandomUrl,CategoriaUrl,RegionUrl,FilterNameUrl,FilterAUrl,FilterCUrl} = getEnvVars();
+const {RandomUrl,CategoriaUrl,FilterAUrl} = getEnvVars();
 
 //Recetas Random
 export const fetcheatWell = async() => {
@@ -23,6 +23,22 @@ export const fetcheatWell = async() => {
 export const ListaCategorias = async() => {
   try{
   const endpoint = CategoriaUrl;
+
+  // Realizar la petición a la API. Esta petición debe ser asíncrona.
+  const response = await fetch(endpoint);
+  const data = await response.json();
+
+  return data;
+  }catch (error) {
+  console.log(error);
+  return { count: 0 };
+  }
+};
+
+//Filtrado areas
+export const FilterArea = async() => {
+  try{
+  const endpoint = FilterAUrl;
 
   // Realizar la petición a la API. Esta petición debe ser asíncrona.
   const response = await fetch(endpoint);
