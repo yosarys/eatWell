0b619b6beb030bf65980eef388e382bcd90a30ad
nicollh9 <nicollarita06@gmail.com>
2021-06-29 22:56:47 -0600

    Map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { StyleSheet, TouchableOpacity, View } from "react-native";
-import Card from "./InfoPantallas";
+import {Card} from './InfoPantallas';
 
-const CardList = ({ randomR }) => {
+export const CardList = ({ randomR }) => {
   return (
     <View style={styles.container}>
       {randomR.meals.map((randomInfo, index) => (
@@ -23,4 +23,3 @@ const styles = StyleSheet.create({
   },
 });
 
-export default CardList;
