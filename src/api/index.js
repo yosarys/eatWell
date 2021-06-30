20197fe6f72a93630ae4fcc96fbf5cfbd133c86e
nicollh9 <nicollarita06@gmail.com>
2021-06-29 22:54:06 -0600

    Importacion y manejo de environment

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,20 @@
-const eatapi = "https://www.themealdb.com/api/json/v1/1/random.php";
+import getEnvVars from "../../environment";
 
+// Rutas de la API
+const {RandomUrl,CategoriaUrl,RegionUrl,FilterNameUrl,FilterAUrl} = getEnvVars();
+
+//Recetas Random
 export const fetcheatWell = async() => {
-    try {
-        const endpoint = eatapi;
+    try{
+    const endpoint = RandomUrl;
 
-        // Realizar la petición a la API. Esta petición debe ser asíncrona.
-        const response = await fetch(endpoint);
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.log(error);
-        return { count: 0 };
+    // Realizar la petición a la API. Esta petición debe ser asíncrona.
+    const response = await fetch(endpoint);
+    const data = await response.json();
+
+    return data;
+    }catch (error) {
+    console.log(error);
+    return { count: 0 };
     }
 };
-
-export default fetcheatWell;
