8eed25030810c69c675e74038b5099139d04757b
nicollh9 <nicollarita06@gmail.com>
2021-06-30 00:04:05 -0600

    Eliminacón de url

--- environment.js
+++ environment.js
@@ -3,14 +3,12 @@ const ENV = {
     dev: {
         RandomUrl: "https://www.themealdb.com/api/json/v1/1/random.php",
         CategoriaUrl: "https://www.themealdb.com/api/json/v1/1/categories.php",
-        RegionUrl: "https://www.themealdb.com/api/json/v1/1/list.php?a=list",
         FilterNameUrl: "https://www.themealdb.com/api/json/v1/1/search.php?s=",
         FilterAUrl: "https://www.themealdb.com/api/json/v1/1/filter.php?a="
     },
     production: {
         RandomUrl: "https://www.themealdb.com/api/json/v1/1/random.php",
         CategoriaUrl: "https://www.themealdb.com/api/json/v1/1/categories.php",
-        RegionUrl: "https://www.themealdb.com/api/json/v1/1/list.php?a=list",
         FilterNameUrl: "https://www.themealdb.com/api/json/v1/1/search.php?s=",
         FilterAUrl: "https://www.themealdb.com/api/json/v1/1/filter.php?a="
     }
