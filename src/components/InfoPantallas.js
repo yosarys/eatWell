e1e06e742b567bde8c1735dbe691352fc702f8c9
Yosary <yl_santosp@unicah.edu>
2021-06-29 23:15:32 -0600

    Informacion de las Categorias

--- src/components/InfoPantallas.js
+++ src/components/InfoPantallas.js
@@ -1,37 +1,51 @@
 import React from "react";
-import {Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import {Image, StyleSheet, Text, View, ScrollView } from "react-native";
 
+//PANTALLA HOME
 export const Card = ({ randomInfo}) => {
 
   return (
-    <ScrollView style={styles.container}>
-    <Text style={styles.title}>{randomInfo.strMeal}</Text>
-    <Image style={styles.image} source={{ uri: randomInfo.strMealThumb }} />
-    <Text>Instrucciones:</Text>
-    <Text>{randomInfo.strInstructions}</Text>
-    <Text>Categoria:</Text>
-    <Text>{randomInfo.strCategory}</Text>
-    <Text>Area:</Text>
-    <Text>{randomInfo.strArea}</Text>
+      <ScrollView style={styles.container}>
+      <Text style={styles.title}>{randomInfo.strMeal}</Text>
+      <Text>Instrucciones: {randomInfo.strInstructions}</Text>
+      <Text>Categoria: {randomInfo.strCategory}</Text>
+      <Text>Area: {randomInfo.strArea}</Text>
+      <View style={styles.image}>
+        <Image style={styles.image} source={{ uri: randomInfo.strMealThumb }} />
+      </View>
     </ScrollView>
   );
 };
 
 
-const styles = StyleSheet.create({
-  container: {
-    padding: 20,
-    margin: 0.5,
-    borderColor: "#ffecb3",
-    backgroundColor: "#bdc3c7",
-  },
-  title: {
-    fontSize: 25,
-    fontWeight: "bold",
-    alignSelf: "center",
-  },
-  image: {
-    width: 300,
-    height: 300,
-  }
-});
+//PANTALLA CATEGORIAS
+export const CateRecetas = ({categoriaInfo}) => {
+return (
+    <ScrollView >
+      <Text>{categoriaInfo.idCategory}</Text>
+      <Text>{categoriaInfo.strCategory}</Text>
+      <Text>{categoriaInfo.strCategoryDescription}</Text>
+      <Image style={styles.image} source={{ uri: categoriaInfo.strCategoryThumb }} />
+    </ScrollView>
+  )};
+
+  
+  const styles = StyleSheet.create({
+    container: {
+      padding: 20,
+      margin: 0.5,
+      borderColor: "#ffecb3",
+      backgroundColor: "#bdc3c7",
+     },
+    title: {
+     fontSize: 25,
+     fontWeight: "bold", 
+     alignSelf: "center",
+    },
+    image: {
+     width: 300,
+     height: 300,
+     }
+    });
+  
+
