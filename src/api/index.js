bf07916d7d51574db761d0368bb64e2f02bf9d89
Yosary <yl_santosp@unicah.edu>
2021-06-29 23:13:17 -0600

    Peticiones de la API

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import getEnvVars from "../../environment";
 
 // Rutas de la API
-const {RandomUrl,CategoriaUrl,RegionUrl,FilterNameUrl,FilterAUrl} = getEnvVars();
+const {RandomUrl,CategoriaUrl,RegionUrl,FilterNameUrl,FilterAUrl,FilterCUrl} = getEnvVars();
 
 //Recetas Random
 export const fetcheatWell = async() => {
@@ -17,4 +17,20 @@ export const fetcheatWell = async() => {
     console.log(error);
     return { count: 0 };
     }
+};
+
+//Lista de categorias
+export const ListaCategorias = async() => {
+  try{
+  const endpoint = CategoriaUrl;
+
+  // Realizar la petición a la API. Esta petición debe ser asíncrona.
+  const response = await fetch(endpoint);
+  const data = await response.json();
+
+  return data;
+  }catch (error) {
+  console.log(error);
+  return { count: 0 };
+  }
 };
