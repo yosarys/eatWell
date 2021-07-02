03d560f37aa7b8dc764f7a2a4670c2b8f11458a8
Yosary <yl_santosp@unicah.edu>
2021-07-02 00:11:01 -0600

    import Filtro

--- src/components/screens/RegionLista.js
+++ src/components/screens/RegionLista.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { ScrollView, StyleSheet, View, SafeAreaView} from "react-native";
 import { RegionR} from '../Map';
+import { FilterArea} from '../../api';
 
 export const RegionScreen = ({navigation}) => {
 const [region, setregiones] = useState();
@@ -18,7 +19,7 @@ const [region, setregiones] = useState();
   return (
     <SafeAreaView>
     <ScrollView>
-      <>{region.meals ? <RegionR area={region} navigation={navigation} /> : null}</>
+    <>{region ? <RegionR area={region} navigation={navigation} /> : null}</>
     </ScrollView>
     </SafeAreaView>
   );
